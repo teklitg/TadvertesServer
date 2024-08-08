d7fe8497ca662a38be33eb1fbabed06c6f102ea5
teklit <teklit.gebrer@gmail.com>
2024-08-08 11:48:57 +0300

    test vercel

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const mongoose = require("mongoose")
-const app = require("./app")
-const {URL, PORT}= require("./utility/utility")
+const mongoose = require("mongoose");
+const app = require("./app");
+const { URL, PORT } = require("./utility/utility");
 
-mongoose.connect(URL)
-.then(()=> console.log("connected"))
-.then(()=> console.log("connected with databas"))
-
-app.listen(PORT,
-  ()=>console.log(`server runing on port ${PORT}`))
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
